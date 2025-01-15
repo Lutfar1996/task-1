1a063b969ec7c75fcad4961dbeda628f8a1fd87b
lutfar1996 <lutfar.rahman569@gmail.com>
2025-01-15 15:53:15 +0600

    Fix the issue in main.tf file

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ resource "digitalocean_kubernetes_cluster" "my_cluster" {
   
   node_pool {
     name       = "default-node-pool"
-    size       = var.k8s_node_size  # Node size from variables
-    node_count = var.k8s_node_count  # Node count from variables
+    size       = var.k8s_node_size
+    node_count = var.k8s_node_count
   }
 }
 
 resource "digitalocean_droplet" "example" {
   name   = "example-droplet"
   region = "nyc3"
-  size   = var.droplet_size  # Droplet size from variables
-  image  = "ubuntu-20-04-x64"  # Base Ubuntu image
+  size   = var.droplet_size
+  image  = "ubuntu-20-04-x64"
 }
 
 output "cluster_id" {
