9b3635f9c81267b17a23d324ea5de847c3abaa7a
lutfar1996 <lutfar.rahman569@gmail.com>
2025-01-15 16:05:00 +0600

    Add the backend file

--- backend.tf
+++ backend.tf
@@ -4,8 +4,6 @@ terraform {
     key            = "terraform/state/terraform.tfstate"  # Path where the state file will be stored
     region         = "nyc3"  # DigitalOcean Spaces region (NYC3)
     endpoint       = "https://nyc3.digitaloceanspaces.com"  # Endpoint for DigitalOcean Spaces API
-    access_key     = var.dos_access_key  # Access key stored in variables
-    secret_key     = var.dos_secret_key  # Secret key stored in variables
-    acl            = "private"  # Set to private for security
+   acl            = "private"  # Set to private for security
   }
 }
