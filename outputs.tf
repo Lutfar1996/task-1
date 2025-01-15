e950547461e60c0b1f193c9543df73b3d9661ab6
lutfar1996 <lutfar.rahman569@gmail.com>
2025-01-15 15:57:22 +0600

    Remove outputs.tf from flarie-iac branch

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_id" {
-  description = "The ID of the Kubernetes cluster"
-  value       = digitalocean_kubernetes_cluster.my_cluster.id
-}
-
-output "droplet_ip" {
-  description = "The public IP address of the created droplet"
-  value       = digitalocean_droplet.example.ipv4_address
-}
