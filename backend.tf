589fb48af47d78ffcd3b335b63e405dd4f652001
lutfar1996 <lutfar.rahman569@gmail.com>
2025-01-15 16:17:12 +0600

    Recreate the backend file

--- backend.tf
+++ backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "flarie"  # The name of your DigitalOcean Spaces bucket
     key            = "terraform/state/terraform.tfstate"  # Path where the state file will be stored
-    region         = "nyc3"  # DigitalOcean Spaces region (NYC3)
+    # region         = "nyc3"  # DigitalOcean Spaces region (NYC3)
+    region         =  "ap-southeast-1"
     endpoint       = "https://nyc3.digitaloceanspaces.com"  # Endpoint for DigitalOcean Spaces API
    acl            = "private"  # Set to private for security
   }
