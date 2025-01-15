79f532d9f555c5a7b515248debfd79de4a2794ce
lutfar1996 <lutfar.rahman569@gmail.com>
2025-01-15 16:03:40 +0600

    Add the Variables file

--- variables.tf
+++ variables.tf
@@ -1,9 +1,23 @@
-variable "digitalocean_token" {
-  description = "The DigitalOcean API Token"
+variable "dos_api_token" {
+  description = "DigitalOcean API token"
   type        = string
 }
 
-variable "ssh_key_id" {
-  description = "The SSH key ID to be used for Droplet creation"
+
+variable "k8s_node_count" {
+  description = "Number of nodes in the Kubernetes cluster"
+  type        = number
+  default     = 2
+}
+
+variable "k8s_node_size" {
+  description = "Size of each node in the Kubernetes cluster"
   type        = string
+  default     = "s-2vcpu-4gb"  # Example size
+}
+
+variable "droplet_size" {
+  description = "Size of the Droplet"
+  type        = string
+  default     = "s-1vcpu-1gb"  # Example size
 }
