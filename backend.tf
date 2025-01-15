5b4119b4b3723a3111fcb5ca09ff9157570a68f3
lutfar1996 <lutfar.rahman569@gmail.com>
2025-01-15 16:26:05 +0600

    CHange the path file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "flarie"  # The name of your DigitalOcean Spaces bucket
-    key            = "terraform/state/terraform.tfstate"  # Path where the state file will be stored
+    key            = "terraform.tfstate"  # Path where the state file will be stored
     # region         = "nyc3"  # DigitalOcean Spaces region (NYC3)
     region         =  "ap-southeast-1"
     endpoint       = "nyc3.digitaloceanspaces.com"  # Endpoint for DigitalOcean Spaces API
